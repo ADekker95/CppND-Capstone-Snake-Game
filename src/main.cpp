fcfd66cad0840c09e33ee50fba33ca9c0d11b0fb
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-11 19:28:50 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,15 @@ int main() {
   constexpr std::size_t kGridWidth{32};
   constexpr std::size_t kGridHeight{32};
 
+  int nSnakes = 2;
   Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
   Controller controller;
   Game game(kGridWidth, kGridHeight);
-  game.Run(controller, renderer, kMsPerFrame);
+  game.Run(controller, renderer, kMsPerFrame, nSnakes);
   std::cout << "Game has terminated successfully!\n";
-  std::cout << "Score: " << game.GetScore() << "\n";
-  std::cout << "Size: " << game.GetSize() << "\n";
+  // determine winning snake 
+  int snakeIndex = game.GetIndexWinningSnake();
+  std::cout << "Score: " << game.GetScoreWinningSnake(snakeIndex) << "\n";
+  std::cout << "Size: " << game.GetSizeWinningSnake(snakeIndex) << "\n";
   return 0;
 }
