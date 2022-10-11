41f1a90103a76b07ac109072785504480f4546c0
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-11 19:29:59 +0200

    Update controller.h

--- src/controller.h
+++ src/controller.h
@@ -3,6 +3,22 @@
 
 #include "snake.h"
 
+// keys to manage snake 1
+enum ControlKeys1 {
+  SDLK_UP,
+  SDLK_DOWN,
+  SDLK_LEFT,
+  SDLK_RIGHT  
+} 
+
+// keys to manage snake 2
+enum ControlKeys2 {
+  SDLK_w,
+  SDLK_a,
+  SDLK_s,
+  SDLK_d
+}
+
 class Controller {
  public:
   void HandleInput(bool &running, Snake &snake) const;
