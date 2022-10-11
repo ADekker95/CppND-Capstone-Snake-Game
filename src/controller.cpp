53d4f14daa2a5634b6cfd52d3b9aacc3132b2111
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-11 19:30:18 +0200

    Update controller.cpp

--- src/controller.cpp
+++ src/controller.cpp
@@ -9,29 +9,36 @@ void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
   return;
 }
 
-void Controller::HandleInput(bool &running, Snake &snake) const {
+void Controller::HandleInput(bool &running, std::vector<<Snake>> &snakes) const {
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
       running = false;
     } else if (e.type == SDL_KEYDOWN) {
       switch (e.key.keysym.sym) {
-        case SDLK_UP:
+        // determine snake, arrows for snake 1, wasd for snake 2
+        if (e.key.keysym.sym in ControlKeys1){
+          snake = snakes[0]
+        else: 
+          snake = snakes[1]
+        }
+       
+        case (SDLK_UP or SDLK_w):
           ChangeDirection(snake, Snake::Direction::kUp,
                           Snake::Direction::kDown);
           break;
 
-        case SDLK_DOWN:
+        case (SDLK_DOWN or SDLK_s):
           ChangeDirection(snake, Snake::Direction::kDown,
                           Snake::Direction::kUp);
           break;
 
-        case SDLK_LEFT:
+        case (SDLK_LEFT or SDLK_a):
           ChangeDirection(snake, Snake::Direction::kLeft,
                           Snake::Direction::kRight);
           break;
 
-        case SDLK_RIGHT:
+        case (SDLK_RIGHT or SDLK_d):
           ChangeDirection(snake, Snake::Direction::kRight,
                           Snake::Direction::kLeft);
           break;
