4330004934ca84bff7fbbe025135590f41553f99
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-13 11:47:25 +0200

    Update controller.h

--- src/controller.h
+++ src/controller.h
@@ -1,20 +1,28 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
-
+#include "game.h"
 #include "snake.h"
 #include "SDL.h"
 #include <set>
 
 class Controller {
  public:
-  // TODO how to initialize these sets more elegantly
-  // keys to manage snake 1
-  std::set<SDL_Keycode> control_set_snake1; // ({SDLK_UP, SDLK_DOWN, SDLK_LEFT, SDLK_RIGHT});
-  // keys to manage snake 2
-  std::set<SDL_Keycode> control_set_snake2; // ({SDLK_w, SDLK_a, SDLK_s, SDLK_d}); 
+  Controller(const SDL_Keycode up, const SDL_Keycode down, const SDL_Keycode left, const SDL_Keycode right):
+   	up_(up),
+    down_(down),
+    left_(left),
+    right_(right)
+    {
+    };
+  
+  ~Controller();
   void HandleInput(bool &running, Snake &snake) const;
 
  private:
+  SDL_Keycode up_;
+  SDL_Keycode down_;
+  SDL_Keycode left_;
+  SDL_Keycode right_;
   void ChangeDirection(Snake &snake, Snake::Direction input,
                        Snake::Direction opposite) const;
 };
