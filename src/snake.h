a1fddd53080f164b2a860ba1aa8f33848c43c107
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-15 21:41:07 +0200

    Update snake.h

--- src/snake.h
+++ src/snake.h
@@ -8,13 +8,11 @@ class Snake {
  public:
   enum class Direction { kUp, kDown, kLeft, kRight };
 
-  //Initalize the snake at a random place
-  Snake(std::size_t grid_width, std::size_t grid_height, int start_head_x, int start_head_y)
+  Snake(std::size_t grid_width, std::size_t grid_height)
       : grid_width(grid_width),
-        grid_height(grid_height),
-        head_x(start_head_x),
-        head_y(start_head_y) {}
+        grid_height(grid_height) {}
 
+  
   void Update();
 
   void GrowBody();
@@ -24,12 +22,14 @@ class Snake {
 
   float speed{0.1f};
   int size{1};
+  float head_x;
+  float head_y;
   // instead of a game score, each snake now has a score
   int score{0};
   bool alive{true};
-  float head_x;
-  float head_y;
   std::vector<SDL_Point> body;
+  void SetHead(float x, float y);
+
 
  private:
   void UpdateHead();
@@ -38,6 +38,7 @@ class Snake {
   bool growing{false};
   int grid_width;
   int grid_height;
+
 };
 
 #endif
