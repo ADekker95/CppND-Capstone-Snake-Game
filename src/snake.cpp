f1189d85ea95eda251c6c8db422b5abf85edd5f0
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-16 21:45:28 +0200

    Update snake.cpp

--- src/snake.cpp
+++ src/snake.cpp
@@ -2,7 +2,12 @@
 #include <cmath>
 #include <iostream>
 
-void Snake::Update() {
+void Snake::PlaceSnakeHead(float x, float y){
+  head_x = x;
+  head_y = y;
+}
+
+void Snake::Update(Snake const &CompetitorSnake) {
   SDL_Point prev_cell{
       static_cast<int>(head_x),
       static_cast<int>(
@@ -15,14 +20,9 @@ void Snake::Update() {
   // Update all of the body vector items if the snake head has moved to a new
   // cell.
   if (current_cell.x != prev_cell.x || current_cell.y != prev_cell.y) {
-    UpdateBody(current_cell, prev_cell);
+    UpdateBody(current_cell, prev_cell, CompetitorSnake);
   }
 }
-
-void Snake::SetHead(float x, float y) {
-  head_x = x;
-  head_y = y;
-} 
 
 void Snake::UpdateHead() {
   switch (direction) {
@@ -48,7 +48,7 @@ void Snake::UpdateHead() {
   head_y = fmod(head_y + grid_height, grid_height);
 }
 
-void Snake::UpdateBody(SDL_Point &current_head_cell, SDL_Point &prev_head_cell) {
+void Snake::UpdateBody(SDL_Point &current_head_cell, SDL_Point &prev_head_cell, Snake const &CompetitorSnake) {
   // Add previous head location to vector
   body.push_back(prev_head_cell);
 
@@ -61,11 +61,29 @@ void Snake::UpdateBody(SDL_Point &current_head_cell, SDL_Point &prev_head_cell) 
   }
 
   // Check if the snake has died.
+  // Due to collision with itself 
   for (auto const &item : body) {
     if (current_head_cell.x == item.x && current_head_cell.y == item.y) {
       alive = false;
+      std::cout << "Collision with own body \n";
+      return;
     }
   }
+  
+  // Due to collision with the body of other snake 
+  for (auto const &item : CompetitorSnake.body) {
+    if (current_head_cell.x == item.x && current_head_cell.y == item.y) {
+      alive = false;
+      std::cout << "Collision with other snake body \n";
+      return;
+    }
+  }
+  // Due to collision with the head of the other snake 
+  if (current_head_cell.x == static_cast<int>(CompetitorSnake.head_x) && current_head_cell.y == static_cast<int>(CompetitorSnake.head_y)) {
+    alive = false;
+    std::cout << "Collision with other snake head \n";
+    return;
+  } 
 }
 
 void Snake::GrowBody() { growing = true; }
