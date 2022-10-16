9a6f838017c2dde5a41b22be46f34f92e467f398
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-16 21:44:01 +0200

    Update game.h

--- src/game.h
+++ src/game.h
@@ -9,17 +9,19 @@
 
 class Game {
  public:
-  Game(std::size_t grid_width, std::size_t grid_height, std::size_t nSnakes);
+  Game(std::size_t grid_width, std::size_t grid_height, std::size_t allowed_score_delta);
   void Run(Controller const &controller1, Controller const &controller2, Renderer &renderer, std::size_t target_frame_duration);
-  int GetIndexWinningSnake() const;
-  int GetScoreWinningSnake(int index) const;
-  int GetSizeWinningSnake(int index) const;
-  int CalculateScoreDelta() const;
-
+  int GetWinningSnake() const;
+  int GetScore() const;
+  int GetSize() const;
+  void CalculateScoreDelta(Snake const &snake1, Snake const &snake2);
+  // initially the snake have a score difference of 0
+  int score_delta{0};
+  size_t allowed_score_delta;
 
  private:
-  // create a vector of snakes 
-  std::vector<Snake> snakes;
+  Snake snake;
+  Snake snake2;
   SDL_Point food;
 
   std::random_device dev;
@@ -28,7 +30,6 @@ class Game {
   std::uniform_int_distribution<int> random_h;
 
   void PlaceFood();
-  void PlaceSnakes(std::size_t nSnakes); 
   void Update();
 };
 
