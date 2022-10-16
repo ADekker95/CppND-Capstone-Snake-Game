11725d1cc5566d92d617d315872a4105464057f7
ADekker95 <70537263+ADekker95@users.noreply.github.com>
2022-10-16 21:41:51 +0200

    Update controller.h

--- src/controller.h
+++ src/controller.h
@@ -1,11 +1,10 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
+
 #include "snake.h"
-#include "SDL.h"
 
 class Controller {
  public:
-  // Connect user defined control keys to the right private variables 
   Controller(const SDL_Keycode up, const SDL_Keycode down, const SDL_Keycode left, const SDL_Keycode right):
       _Up(up),
       _Down(down),
@@ -15,13 +14,12 @@ class Controller {
   void HandleInput(bool &running, Snake &snake) const;
 
  private:
-  void ChangeDirection(Snake &snake, Snake::Direction input, Snake::Direction opposite) const;
-  // Keys to handle directions of snake 
+  void ChangeDirection(Snake &snake, Snake::Direction input,
+                       Snake::Direction opposite) const;
   SDL_Keycode _Up;
   SDL_Keycode _Down;
   SDL_Keycode _Left;
   SDL_Keycode _Right;
-  
 };
 
 #endif
